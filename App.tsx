1b026e9214e7d7013025c3836fea229fba234c61
rpantalena <33662962+rpantalena@users.noreply.github.com>
2022-11-08 20:43:54 -0500

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const input = '(800)x(600) and (1280)x(960)';
+  const regex = /([0-9]+)\)x\(([0-9]+)/g;
+  const matches = [...input.matchAll(regex)];
+  console.log(matches);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>{matches[0][1]}</p>
     </div>
   );
 }
